2ff7112d3b1cb05d1e6d05ddc8bf3f11c66df8c9
DmitriiGienko <cuanik@mail.ru>
2023-08-20 10:39:38 +0300

    начал создавать UserControllerTest

---
+++ src/test/java/com/skypro/simplebanking/controller/UserControllerTest.java
@@ -0,0 +1,20 @@
+package com.skypro.simplebanking.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserControllerTest {
+
+    @Test
+    void createUser() {
+    }
+
+    @Test
+    void getAllUsers() {
+    }
+
+    @Test
+    void getMyProfile() {
+    }
+}
